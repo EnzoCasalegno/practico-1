bf7aa058602887ddde2caf3b2075a9e05dcfe0c6
EnzoCasalegno <enzocasalegno59@gmail.com>
2024-03-19 17:07:13 -0300

    Update Practico1.java

--- src/ejercicio/txt/Practico1.java
+++ src/ejercicio/txt/Practico1.java
@@ -5,6 +5,9 @@ public class Practico1 {
 
     public static void main(String[] args) {
         System.out.println("Hola Git");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
     
 }
